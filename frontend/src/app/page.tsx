a837049c5ef87361dad578329e332ce9c740f40e
thomas <thomas.chang7@gmail.com>
2025-01-18 14:58:29 -0800

    Add some animations

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { useForm, UseFormReturn } from 'react-hook-form';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -14,8 +14,9 @@ import {
   FormMessage
 } from '@/components/ui/form';
 import * as Fade from '@/components/motion/fade';
-import { useTransition, useState } from 'react';
+import { useTransition, useState, ReactNode } from 'react';
 import { getGifs } from './actions';
+import { AnimatePresence, motion } from 'motion/react';
 
 export const maxDuration = 60;
 
@@ -43,72 +44,141 @@ export default function Home() {
   }
 
   return (
-    <Fade.Container className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md mx-auto rounded-lg shadow-lg p-6">
-        <h1 className="text-2xl font-bold mb-6 text-gray-100">Create GIF</h1>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>GIF Description</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Describe the GIF you want to create"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="youtubeUrl"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>YouTube URL</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter YouTube video URL" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#10a37f] hover:bg-[#1a7f64] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#10a37f]"
-              disabled={isPending}
-            >
-              {isPending ? 'Creating...' : 'Create GIF'}
-            </Button>
-          </form>
-        </Form>
-
+    <AnimatePresence>
+      <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
         {isPending ? (
-          <div className="mt-8 space-y-4">
-            {[1, 2, 3, 4].map((i) => (
-              <div
-                key={i}
-                className="h-48 bg-gray-700 animate-pulse rounded-lg"
-              />
-            ))}
-          </div>
+          <Loading />
         ) : gifUrls.length > 0 ? (
-          <div className="mt-8 space-y-4">
-            {gifUrls.map((url, i) => (
-              <img
-                key={i}
-                src={url}
-                alt={`Generated GIF ${i + 1}`}
-                className="w-full rounded-lg"
-              />
-            ))}
-          </div>
-        ) : null}
+          <Results gifUrls={gifUrls} />
+        ) : (
+          <GifForm form={form} onSubmit={onSubmit} />
+        )}
       </div>
-    </Fade.Container>
+    </AnimatePresence>
+  );
+}
+
+function GifForm({
+  form,
+  onSubmit
+}: {
+  form: UseFormReturn<
+    {
+      description: string;
+      youtubeUrl: string;
+    },
+    unknown,
+    undefined
+  >;
+  onSubmit: (data: {
+    description: string;
+    youtubeUrl: string;
+  }) => Promise<void>;
+}) {
+  return (
+    <motion.div
+      className="max-w-md mx-auto rounded-lg shadow-lg p-6"
+      initial={{ y: 5, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -10, opacity: 0 }}
+    >
+      <h1 className="text-2xl font-bold mb-6 text-gray-100">Create GIF</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>GIF Description</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Describe the GIF you want to create"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="youtubeUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>YouTube URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter YouTube video URL" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#10a37f] hover:bg-[#1a7f64] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#10a37f]"
+          >
+            Create GIF
+          </Button>
+        </form>
+      </Form>
+    </motion.div>
+  );
+}
+
+function Loading() {
+  return (
+    <GridView
+      nodes={[1, 2, 3, 4].map((i) => (
+        <div key={i} className="h-48 bg-gray-700 animate-pulse rounded-lg" />
+      ))}
+    />
+  );
+}
+
+function Results({ gifUrls }: { gifUrls: string[] }) {
+  return (
+    <GridView
+      nodes={gifUrls.map((url, i) => (
+        <div key={i} className="aspect-square overflow-hidden rounded-lg">
+          <img
+            src={url}
+            alt={`Generated GIF ${i + 1}`}
+            className="w-full h-full object-cover"
+          />
+        </div>
+      ))}
+    />
+  );
+}
+
+function GridView({ nodes }: { nodes: ReactNode[] }) {
+  return (
+    <motion.div
+      className="mt-8 grid grid-cols-2 gap-4 max-w-xl mx-auto"
+      variants={{
+        hidden: { opacity: 0 },
+        visible: {
+          opacity: 1,
+          transition: { staggerChildren: 0.02 }
+        }
+      }}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
+      {nodes.map((node, i) => (
+        <motion.div
+          key={i}
+          className="aspect-square overflow-hidden rounded-lg"
+          variants={{
+            hidden: { opacity: 0, y: 5 },
+            visible: { opacity: 1, y: 0 }
+          }}
+        >
+          {node}
+        </motion.div>
+      ))}
+    </motion.div>
   );
 }
